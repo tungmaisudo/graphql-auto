003be245d0da1fad5fbc3a7d252050a68fd114e9
tungmaisudo <mtung280196@gmail.com>
2017-11-01 21:38:28 +0700

    commit PORT

--- app.js
+++ app.js
@@ -157,6 +157,6 @@ app.use('/graphql', graphqlHTTP((request, response, graphQLParams) => {
     })
 }))
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
   console.log('GraphQL server running at port 3000...')
 })
